f3929a2006005f0a05f7f4e08cd3ef734cc3823b
dariuszb <greedo904@gmail.com>
2020-02-13 14:52:47 +0100

    Updates

---
+++ precache-manifest.e85cb09e213b9881fe6fb60cba989126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f287bafd5c693f26521193099149aa8f",
+    "url": "/React_snake_game/index.html"
+  },
+  {
+    "revision": "38d30d546374cb262b8d",
+    "url": "/React_snake_game/static/css/main.61487d3a.chunk.css"
+  },
+  {
+    "revision": "c7c874dc8ea7e288ec56",
+    "url": "/React_snake_game/static/js/2.0eeff546.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_snake_game/static/js/2.0eeff546.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d30d546374cb262b8d",
+    "url": "/React_snake_game/static/js/main.8a547cdc.chunk.js"
+  },
+  {
+    "revision": "7aaeb61d11cbea10e41f",
+    "url": "/React_snake_game/static/js/runtime-main.6aa76660.js"
+  }
+]);
